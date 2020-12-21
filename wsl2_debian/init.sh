f5aee44ea9d0736d980ffe7b47a5ac20fd021eb5
suren <suren-m>
2020-12-21 21:08:53 +0000

    wsl2 initial setup

--- wsl2_debian/init.sh
+++ wsl2_debian/init.sh
@@ -5,7 +5,8 @@
 
 # Pre-requisites
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y curl git python python3 python3-pip ansible 
+# python-apt needed for ansible --check
+sudo apt install -y curl git python python-apt python3 python3-pip ansible 
 
 # backup existing .bashrc
 mkdir -p ~/backup;
@@ -14,6 +15,11 @@ cp ~/.bashrc ~/backup/.bashrc.orig
 # backup /etc/profile
 cp /etc/profile ~/backup/profile.orig
 # copy profile into /etc/profile (fix code not found issue due to path)
-sudo cp ./etc/profile /etc/profile
+sudo cp ./patched/profile /etc/profile
+
+# backup /etc/sudoers
+sudo cp /etc/sudoers ~/backup/sudoers.orig
+# update sudoers file. (or use visudo) (-r--r-----)
+sudo cp ./patched/sudoers /etc/sudoers
 
 echo "done"
